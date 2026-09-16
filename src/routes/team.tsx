de3dcc6bf7ee54a091ea89695b88c59a1c43ab98
jayvee2010 <shah.jayvee@gmail.com>
2026-09-16 17:13:24 +0530

    feat: update Sukhman Kaur quote and photo from spreadsheet

--- src/routes/team.tsx
+++ src/routes/team.tsx
@@ -354,6 +354,9 @@ const teamMembers: TeamMember[] = [
     badgeColor:
       "border-violet-400 text-violet-300 bg-violet-950/60 shadow-[0_0_8px_rgba(167,139,250,0.3)]",
     icon: "📱",
+    quote: "Trust the plot twist✨",
+    photoUrl:
+      "https://lh3.googleusercontent.com/d/141SZWvvpaEmUynD-YFDhSBPSBUnKJfRV",
   },
   {
     id: "editor-01",
